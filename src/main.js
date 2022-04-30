9df0d3c86cad4de78f95b98f98bf5a465a57f74d
ViktoriaMikhaylovskaya <mviktoria065@gmail.com>
2022-04-30 16:06:32 +0300

    Отрисует карточки 5 фильмов

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import NewTaskListFilterView from './view/list-filter-view.js';
 import NewUserNameView from './view/user-name-view.js';
+import NewMovieCardView from './view/movie-card-view';
 import NewButtonShowMore from './view/button-show-more-view.js';
 import NewPopupMovieDetailsView from './view/popup-movie-details-view';
 import {render} from './render.js';
@@ -7,8 +8,12 @@ import {render} from './render.js';
 const siteBodyElement = document.querySelector('body');
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+// const siteFilmsSectionElement = siteMainElement.querySelector('.films');
+// const siteFilmListElement = siteFilmsSectionElement.querySelector('.films-list');
 
 render(new NewUserNameView(), siteHeaderElement);
 render(new NewTaskListFilterView(), siteMainElement);
+render(new NewMovieCardView(), siteMainElement);
+
 render(new NewButtonShowMore(), siteMainElement);
 render(new NewPopupMovieDetailsView(), siteBodyElement);
