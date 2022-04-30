377cadfda155fbf7eb8a328f12c806c59710e094
ViktoriaMikhaylovskaya <mviktoria065@gmail.com>
2022-04-30 21:56:59 +0300

    Отрисует статистику с кол-м фильмов на сайте

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -2,6 +2,7 @@ import NewMovieListView from '../view/movie-list-view';
 import NewButtonShowMoreView from '../view/button-show-more-view.js';
 import NewFilmListExtraView from '../view/top-films-list-view';
 import NewTopCommentedFilmsView from '../view/most-commented-films-view';
+import NewStatisticsView from '../view/statistics-view';
 import NewPopupMovieDetailsView from '../view/popup-movie-details-view';
 import {render} from '../render.js';
 
@@ -9,6 +10,7 @@ const siteBodyElement = document.querySelector('body');
 const siteMainElement = document.querySelector('.main');
 const getFilmSection = () => siteMainElement.querySelector('.films');
 const getFilmList = () => getFilmSection().querySelector('.films-list');
+const siteFooterElement = document.querySelector('.footer__statistics');
 
 
 export default class ContentPresenter {
@@ -18,6 +20,7 @@ export default class ContentPresenter {
     render(new NewButtonShowMoreView(), getFilmList());
     render(new NewFilmListExtraView(), getFilmSection());
     render(new NewTopCommentedFilmsView(), getFilmSection());
+    render(new NewStatisticsView(), siteFooterElement);
     render(new NewPopupMovieDetailsView(), siteBodyElement);
   };
 }
