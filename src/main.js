da26199e48c6f3b114794a8c325c5d10e419e80d
ViktoriaMikhaylovskaya <mviktoria065@gmail.com>
2022-04-30 15:34:06 +0300

    Отрисует попап с подробной информацией о фильме

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import NewTaskListFilterView from './view/list-filter-view.js';
 import NewUserNameView from './view/user-name-view.js';
 import NewButtonShowMore from './view/button-show-more-view.js';
+import NewPopupMovieDetailsView from './view/popup-movie-details-view';
 import {render} from './render.js';
 
+const siteBodyElement = document.querySelector('body');
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 
 render(new NewUserNameView(), siteHeaderElement);
 render(new NewTaskListFilterView(), siteMainElement);
 render(new NewButtonShowMore(), siteMainElement);
+render(new NewPopupMovieDetailsView(), siteBodyElement);
