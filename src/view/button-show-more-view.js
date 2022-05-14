c05accc7b602fadc2742918df868572a68a474e2
ViktoriaMikhaylovskaya <mviktoria065@gmail.com>
2022-05-14 19:51:36 +0300

    Добавит наследование простым компонентам

--- src/view/button-show-more-view.js
+++ src/view/button-show-more-view.js
@@ -1,23 +1,9 @@
-import {createElement} from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 const createNewButtonShowMoreTemplate = () => '<button class="films-list__show-more">Show more</button>';
 
-export default class ButtonShowMoreView {
-  #element = null;
-
+export default class ButtonShowMoreView extends AbstractView {
   get template() {
     return createNewButtonShowMoreTemplate();
-  }
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
   }
 }
